5725f7ca2663e90ff91c764ac353a6b30d547b00
Romain Bouyé <romain@bouye.fr>
2017-04-07 12:18:31 +0100

    [php impl]: Add simple ajax request to load user info

--- php/public_html/index.php
+++ php/public_html/index.php
@@ -24,20 +24,44 @@ $loginURL = AUTH_API_BASE_URI
 <html>
 <head>
   <title>MyMiniFactory</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
-
+<body>
 <?php
 
 if(empty($_GET['access_token'])){
   ?>
   <a href="<?php echo $loginURL; ?>">Login With MyMiniFactory</a>
-</html>
   <?php
 } else {
 ?>
   <p>Confgratulation you are logged in to MyMiniFactory API !</p>
-  <p>Access Token: <?php $_GET['access_token'] ?></p>
-  <p><a href="<?php echo $indexURI ?>" >Log out</a></p>
-  </html>
+  <p>Access Token: <?php echo $_GET['access_token']; ?></p>
+  <div class="user-info" style="display:none;">
+    <p>Connected User Info:</p>
+  </div>
+  <p><a href="<?php echo $indexURI; ?>" >Log out</a></p>
+  <script>
+  var accessToken = "<?php echo $_GET['access_token']; ?>";
+  $.ajax({
+         url: "https://www.myminifactory.com/api/v2/user",
+         headers: { 'Authorization': 'Bearer ' + accessToken },
+         type: "GET",
+         success: function(response) {
+           $('.user-info').append('<img src="'+response.avatar_thumbnail_url+'" width=100 /><p><a href="'+response.profile_url+'" >username: ' + response.username + '</a></p>');
+           $('.user-info').show();
+         }
+      });
+  // $.ajax({
+  //        url: "https://www.myminifactory.com/api/v2/user?access_token="+accessToken,
+  //        type: "GET",
+  //        success: function(response) {
+  //          $('.user-info').append('<img src="'+response.avatar_thumbnail_url+'" width=100 /><p><a href="'+response.profile_url+'" >username: ' + response.username + '</a></p>');
+  //          $('.user-info').show();
+  //        }
+  //     });
+  </script>
 <?php
 }
+?>
+</body></html>
